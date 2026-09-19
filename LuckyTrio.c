3d97f379db9127d2ff71890e1ab5ed5c58f2e015
King_In_Yellow <vadim090207@rambler.ru>
2026-09-19 20:57:25 +0330

    Add files via upload

---
+++ LuckyTrio.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int A;
+
+	puts("Введите номер первого игрока: ");
+
+	scanf("%d", &A);
+
+	int B;
+
+	puts("Введите номер второго игрока: ");
+
+	scanf("%d", &B);
+
+	int C;
+
+	puts("Введите номер третьего игрока: ");
+
+	scanf("%d", &C);
+
+	if ((A + B + C) % 3 == 0)
+	{
+		puts("Тройка является счастливой!");
+	}
+	else
+	{
+		puts("Тройка не является счастливой!");
+	}
+
+	return 0;
+}
